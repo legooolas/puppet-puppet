8f3b0f1887401a88d0482e636c50ee45657330f6
David Gardner <git@icmfp.com>
2016-05-06 14:53:09 +0100

    Use correct variable to check whether puppetserver should be running or not.

--- manifests/server/service.pp
+++ manifests/server/service.pp
@@ -31,7 +31,7 @@ class puppet::server::service(
 
   if $puppetserver != undef {
     # Ugh what a hack to avoid duplicate resources, defined() or ensure_resources...:
-    if $ps_ensure {
+    if $puppetmaster {
       class { '::puppetserver_wrapper::service::running': }
     }
     else {
